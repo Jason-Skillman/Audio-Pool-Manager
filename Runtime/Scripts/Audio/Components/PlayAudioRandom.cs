d31961688524d45ff7b1d4c62a67d086f1b3f35b
Jason Skillman <jason_skillman@outlook.com>
2022-11-28 18:08:13 -0500

    Update play audio random

--- Runtime/Scripts/Audio/Components/PlayAudioRandom.cs
+++ Runtime/Scripts/Audio/Components/PlayAudioRandom.cs
@@ -1,7 +1,8 @@
 ﻿namespace AudioPool.Components {
 	using UnityEngine;
 	using AudioConfiguration = AudioConfiguration;
-	
+	using Random = UnityEngine.Random;
+
 	public class PlayAudioRandom : MonoBehaviour {
 		
 		[SerializeField]
@@ -10,9 +11,46 @@
 		private AudioConfiguration audioConfig = default;
 		[SerializeField]
 		private bool use3DPosition = default;
+		[SerializeField]
+		private bool playOnStart = true;
+		[SerializeField]
+		private bool continuousPlay;
+		[SerializeField]
+		private float delay, interval = 1;
+
+		private bool isContinuousPlayRunning;
+
+		private void Start()
+		{
+			if(playOnStart)
+				Play();
+		}
 
 		[ContextMenu("Play")]
 		public void Play() {
+			if(continuousPlay)
+				StartContinuousPlay();
+			else
+				PlayAudio();
+		}
+		
+		[ContextMenu("StartContinuousPlay")]
+		public void StartContinuousPlay()
+		{
+			if(isContinuousPlayRunning) return;
+			
+			isContinuousPlayRunning = true;
+			InvokeRepeating(nameof(PlayAudio), delay, interval);
+		}
+		
+		[ContextMenu("StopContinuousPlay")]
+		public void StopContinuousPlay() {
+			isContinuousPlayRunning = false;
+			CancelInvoke(nameof(PlayAudio));
+		}
+
+		private void PlayAudio()
+		{
 			int randomIndex = Random.Range(0, audioClips.Length);
 			AudioClip audioClip = audioClips[randomIndex];
 			
